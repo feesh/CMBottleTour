661a88520224172ad3b289d7468e006aedab2b74
Trish Ang <trish@counsyl.com>
2015-10-10 22:54:17 -0700

    Added minifier.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
 var nano = require('gulp-cssnano');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var path = require('path');
 
@@ -19,7 +21,10 @@ gulp.task('sass', function() {
       browsers: ['last 2 versions'],
       cascade: false
     }))
+    .pipe(gulp.dest(dirs.dist))
     .pipe(nano())
+    .pipe(minify({compatibility: 'ie8'}))
+    .pipe(rename({extname: ".min.css"}))
     .pipe(gulp.dest(dirs.dist));
 });
 
